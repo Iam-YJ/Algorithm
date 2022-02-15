b4ca8551f358ae43bfaa806d5f45e761abd120e5
Iam-YJ <yujin353@naver.com>
2022-02-16 04:04:14 +0900

    codeup

---
+++ CodeUp/main.py
@@ -0,0 +1,125 @@
+# 6001
+print("Hello")
+
+# 6002
+print("Hello World")
+
+# 6003
+print("Hello")
+print("World")
+
+# 6004
+print("'Hello'")
+
+# 6005
+print('"Hello World"')
+
+# 6006
+print('"' + "!@#$%^&*()'")
+
+# 6007
+print("\"C:\\Download\\'hello'.py\"")
+
+# 6008
+print("print(\"Hello\\nWorld\")")
+
+# 6009
+c = input()
+print(c)
+
+# 6010
+c = input()
+print(int(c))
+
+# 6011
+f = input()
+f = float(f)
+print(f)
+
+# 6012
+a = input()
+b = input()
+print(a)
+print(b)
+
+# 6013
+a = input()
+b = input()
+print(b)
+print(a)
+
+# 6014
+f = input()
+f = float(f)
+print(f)
+print(f)
+print(f)
+
+# 6015
+a, b = input().split()
+print(a)
+print(b)
+
+# 6016
+a, b = input().split()
+print(b, a)
+
+# 6017
+s = input()
+print(s, s, s)
+
+# 6018
+h, m = input().split()
+print(h, m, sep=":")
+
+# 6019
+y, m, d = input().split(".")
+print(d, m, y, sep="-")
+
+# 6020
+f, b = input().split("-")
+print(f, b, sep="")
+
+# 6021
+s = input()
+str = list(s)
+
+for c in str:
+    print(c)
+
+# 6022
+s = input()
+print(s[:2], s[2:4], s[4:])
+
+# 6023
+h, m, s = input().split(":")
+print(m)
+
+# 6024
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
+
+# 6025
+a, b = input().split()
+c = int(a) + int(b)
+print(c)
+
+# 6026
+a, b = input().split()
+c = float(a) + float(b)
+print(c)
+
+# 6027
+a = input()
+n = int(a)
+print('%x'% n)
+
+# 6028
+a = input()
+n = int(a)
+print('%x'% n)
+
+# 6029
+# 6030
+
