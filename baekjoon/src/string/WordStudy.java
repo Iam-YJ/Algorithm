68acf367068e89ea117b44592d99ea92b105f6ae
PYJ <yujin353@naver.com>
2022-04-28 19:39:48 +0900

    2022-04-28 algorithm

--- baekjoon/src/string/WordStudy.java
+++ baekjoon/src/string/WordStudy.java
@@ -3,45 +3,39 @@ package string;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class WordStudy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.next().toUpperCase();
+        String word = scanner.nextLine();
+        word = word.toUpperCase();
         List<Character> wordList = new ArrayList<>();
+        Character result = null;
 
-        for(Character character : word.toCharArray()){
+        for (Character character : word.toCharArray()) {
             wordList.add(character);
         }
 
         Map<Character, Integer> wordCount = new HashMap<>();
-        for(Character character : wordList){
-            wordCount.put(character, wordCount.getOrDefault(character, 0)+1);
+        for (Character character : wordList) {
+            wordCount.put(character, wordCount.getOrDefault(character, 0) + 1);
         }
 
         Integer max = Collections.max(wordCount.values());
         int count = 0;
-        for(Integer integer : wordCount.values()){
-            if(integer == max) {
-                count ++;
+
+        for (Character character : wordCount.keySet()) {
+            if (max.equals(wordCount.get(character))) {
+                result = character;
+                count++;
+            }
+            if (count >= 2) {
+                result = '?';
             }
         }
-
-        if(count >= 2){
-            System.out.println("?");
-            return;
-        }
-
-        for(Character character : wordCount.keySet()){
-            if(max.equals(wordCount.get(character))){
-                System.out.println(character);
-                return;
-            }
-        }
+        System.out.println(result);
     }
 }
