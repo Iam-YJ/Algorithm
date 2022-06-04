7810d514866beea86d6047ae4519d7b6ffcaf9db
PYJ <yujin353@naver.com>
2022-06-04 21:21:18 +0900

    algorithm

---
+++ baekjoon/src/basicmathone/MathFractions2.java
@@ -0,0 +1,14 @@
+package basicmathone;
+
+import java.util.Scanner;
+
+public class MathFractions {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+
+
+    }
+}
