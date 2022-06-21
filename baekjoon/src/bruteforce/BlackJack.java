64481b0855e1dcae3af50272a77be0cebe8363ba
PYJ <yujin353@naver.com>
2022-06-21 20:57:52 +0900

    [20220621] baekjoon 2798

---
+++ baekjoon/src/bruteforce/BlackJack.java
@@ -0,0 +1,51 @@
+package bruteforce;
+
+import java.util.Scanner;
+
+public class BlackJack {
+    public static int tempNum = 0;
+    public static int min = 0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[] cards = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            cards[i] = scanner.nextInt();
+        }
+        BlackJack(M, cards);
+    }
+
+    public static void BlackJack(int M, int[] cards) {
+        // M을 넘지 않으면서 M에 최대한 가까운 카드 3장의 합 출력
+        BlackJack.min = Math.abs((cards[0] + cards[1] + cards[2]) - M);
+        BlackJack.tempNum = (cards[0] + cards[1] + cards[2]);
+
+        for (int i = 0; i < cards.length - 2; i++) {
+            for (int j = i + 1; j < cards.length - 1; j++) {
+                for (int k = j + 1; k < cards.length; k++) {
+                    Sum(cards[i], cards[j], cards[k], M);
+//                    System.out.println("count : " + count + " " + i + " " + j + " " + k);
+//                    System.out.println("i j k " + cards[i] + " " + cards[j] + " " + cards[k]);
+                }
+            }
+        }
+        System.out.println(BlackJack.tempNum);
+    }
+
+    public static void Sum(int numA, int numB, int numC, int M) {
+        int sum = numA + numB + numC;
+
+        if (sum == M) {
+            BlackJack.tempNum = sum;
+        }
+
+        if (sum < M && Math.abs(M - sum) < BlackJack.min) {
+            BlackJack.min = Math.abs(M - sum);
+            BlackJack.tempNum = sum;
+        }
+    }
+}
--- baekjoon/src/recursive/Star.java
+++ baekjoon/src/recursive/Star.java
@@ -10,42 +10,38 @@ public class Star {
         int num = Integer.parseInt(bf.readLine());
 
         StarPrint(num);
-
     }
 
-    public static void StarPrint(int num){
-        int count = num / 3;
-        int blankNum = 0;
-        for (int i = 0; i < count ; i++) {
-            for (int j = 0; j < count; j++) {
-                if(blankNum == 4){
-                    Blank(count);
-                }
-                ThreeStar(num, count);
-                blankNum ++;
-            }
-        }
-    }
-
-    public static void ThreeStar(int num, int count){ // 9 3
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if(i == 1 && j ==1){
-                    System.out.print(" ");
-                }else{
-                    System.out.print("*");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void Blank(int num){
+    public static void StarPrint(int num) {
+        int count = num / 3; // 3의 제곱
+        int blankNum = 0; // 4일 때 공백
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
-                System.out.print(" ");
+                if (blankNum == 4) {
+                    Blank(count);
+                } else {
+                    BasicStar(1);
+                }
+                blankNum++;
             }
             System.out.println();
+        }
+    }
+
+    public static void BasicStar(int num){
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print("*");
+            }
+        }
+        System.out.print("*");
+    }
+
+    public static void Blank(int count) {
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print(" ");
+            }
         }
     }
 }
