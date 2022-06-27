e0c564107839589d791660a9eac55c7975591eeb
PYJ <yujin353@naver.com>
2022-06-27 20:51:24 +0900

    [20220627] baekjoon 2751

--- baekjoon/src/sort/NumberSortTwo.java
+++ baekjoon/src/sort/NumberSortTwo.java
@@ -2,12 +2,14 @@ package sort;
 
 import javax.imageio.metadata.IIOMetadataFormatImpl;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class NumberSortTwo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        StringBuilder stringBuilder = new StringBuilder();
         int num = scanner.nextInt();
 
         List<Integer> numArr = new ArrayList<>();
@@ -16,12 +18,13 @@ public class NumberSortTwo {
             numArr.add(scanner.nextInt());
         }
 
+        Collections.sort(numArr);
 //        numArr = Quick(numArr);
-        numArr = Merge(numArr);
 //
-//        for (int i = 0; i < numArr.size(); i++) {
-//            System.out.println(numArr.get(i));
-//        }
+        for (int i = 0; i < numArr.size(); i++) {
+            stringBuilder.append(numArr.get(i)).append("\n");
+        }
+        System.out.println(stringBuilder);
     }
 
     public static List<Integer> Quick(List<Integer> list) {
@@ -51,35 +54,6 @@ public class NumberSortTwo {
         joined.add(pivot);
         joined.addAll(right);
         return joined;
-    }
-
-    public static List<Integer> Merge(List<Integer> list){
-        List<Integer> answer = new ArrayList<>();
-
-        if(list.size() == 0 || list.size() == 1){
-            return list;
-        }
-
-        List<Integer> left = new ArrayList<>();
-        List<Integer> right = new ArrayList<>();
-
-        int range = list.size()/2;
-        for (int i = 0; i < range; i++) {
-            left.add(list.get(i));
-        }
-        for (int i = range; i < list.size(); i++) {
-            right.add(list.get(i));
-        }
-
-        for (int i = 0; i < left.size(); i++) {
-            if(left.get(i) < right.get(i)){
-                answer.add(left.get(i));
-            }else{
-                answer.add(right.get(i));
-            }
-        }
-        System.out.println(answer);
-        return answer;
     }
 
 }
