a1f5f385f4bb00343d1a5a29dcd344824d0ef2ca
PYJ <yujin353@naver.com>
2022-09-12 15:59:00 +0900

    [20220912] baekjoon 1181

--- baekjoon/src/sort/WordSort.java
+++ baekjoon/src/sort/WordSort.java
@@ -1,4 +1,30 @@
 package sort;
 
+import java.util.*;
+
 public class WordSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        Set<String> wordSet = new HashSet<>();
+
+        for (int i = 0; i < count; i++) {
+            wordSet.add(scanner.next());
+        }
+        String[] wordArray = new String[wordSet.size()];
+
+        wordArray = wordSet.toArray(new String[0]);
+
+        Arrays.sort(wordArray, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() != o2.length() ? o1.length() - o2.length() : o1.compareTo(o2);
+            }
+        });
+
+        // answer print
+        for (int i = 0; i < wordArray.length; i++) {
+            System.out.println(wordArray[i]);
+        }
+    }
 }
