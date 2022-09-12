adbf518ac993c24b61c41d44687be592b0f174ae
PYJ <yujin353@naver.com>
2022-09-12 15:28:36 +0900

    [20220912] baekjoon 11650

---
+++ baekjoon/src/sort/coordinate.java
@@ -0,0 +1,31 @@
+package sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class coordinate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        int arrays[][] = new int[number][2];
+
+        for (int i = 0; i < number; i++) {
+            arrays[i][0] = scanner.nextInt();
+            arrays[i][1] = scanner.nextInt();
+        }
+
+        Arrays.sort(arrays, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0]!=o2[0] ? o1[0]-o2[0] : o1[1]-o2[1];
+            }
+        });
+
+        for (int i = 0; i < number; i++) {
+            System.out.println(arrays[i][0] + " " + arrays[i][1]);
+        }
+
+    }
+}
