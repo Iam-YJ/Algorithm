e2eb62e1db600b7ec2702e4e32870d5b696925cb
PYJ <yujin353@naver.com>
2022-11-04 22:30:42 +0900

    [20221104] baekjoon 24479

--- baekjoon/src/graph/DFS1.java
+++ baekjoon/src/graph/DFS1.java
@@ -1,14 +1,14 @@
 package graph;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class DFS1 {
 
-    static ArrayList<Integer>[] graph;
-    static boolean[] visited;
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int[] visited;
+    static int count; // 방문 순서
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -16,35 +16,39 @@ public class DFS1 {
         int m = scanner.nextInt();
         int r = scanner.nextInt();
 
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
+        visited = new int[n + 1];
 
         for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+            graph.add(new ArrayList<>());
         }
 
         for (int i = 0; i < m; i++) {
             int start = scanner.nextInt();
             int end = scanner.nextInt();
-            graph[start].add(end);
-            graph[end].add(start);
+            graph.get(start).add(end);
+            graph.get(end).add(start);
         }
 
         for (int i = 0; i <= n; i++) {
-            Collections.sort(graph[i]);
+            Collections.sort(graph.get(i));
         }
 
+        count = 1;
+
         dfs(r);
-        System.out.println(0);
+
+        for (int i = 1; i < visited.length; i++) {
+            System.out.println(visited[i]);
+        }
 
     }
 
-    public static void dfs(int r){
-        visited[r] = true;
-        System.out.println(r);
+    public static void dfs(int r) {
+        visited[r] = count;
 
-        for(int i : graph[r]){
-            if(!visited[i]){
+        for (int i : graph.get(r)) {
+            if (visited[i] == 0) {
+                count += 1;
                 dfs(i);
             }
 
