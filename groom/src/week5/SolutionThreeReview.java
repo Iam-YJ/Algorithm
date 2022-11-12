9f92dfeecd92f2d849a605aa1c40d48cd41c720c
PYJ <yujin353@naver.com>
2022-11-12 16:28:23 +0900

    [20221112] groom week 5 review

--- groom/src/week5/SolutionThreeReview.java
+++ groom/src/week5/SolutionThreeReview.java
@@ -12,8 +12,8 @@ public class SolutionThreeReview {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         numCount = scanner.nextInt();
-        numList = new int[numCount+1];
-        permutedList = new int[numCount+1];
+        numList = new int[numCount + 1];
+        permutedList = new int[numCount + 1];
         visited = new boolean[numCount + 1];
 
         for (int i = 0; i < numCount; i++) {
@@ -21,11 +21,26 @@ public class SolutionThreeReview {
         }
 
         Arrays.sort(numList);
-        Permutation(0);
+        Permutation(0, numList.length, 4);
     }
 
-    public static void Permutation(int n) {
+    public static void Permutation(int depth, int n, int r) {
+        if (depth == r) {
+            for (int i = 0; i < permutedList.length; i++) {
+                System.out.println(permutedList[i]);
+            }
+            return;
+        }
 
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                permutedList[depth] = numList[i];
+                Permutation(depth + 1, n, r);
+                permutedList[depth] = 0;
+                visited[i] = false;
+            }
+        }
 
     }
 }
