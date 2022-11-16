9fd204e64d1cfbdf1f9658c523a210aef4339649
PYJ <yujin353@naver.com>
2022-11-16 21:18:01 +0900

    [20221116] groom week6 review

--- groom/src/week6/SolutionThreeReveiw.java
+++ groom/src/week6/SolutionThreeReveiw.java
@@ -1,7 +1,21 @@
 package week6;
 
+import java.util.Scanner;
+
 public class SolutionThreeReveiw {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 0; i < num; i++) {
+            String str = scanner.nextLine();
+            String key = scanner.next();
+            letter(str, key);
+        }
 
+    }
+
+    public static void letter(String str, String key) {
+        System.out.println(str);
+        System.out.println(key);
     }
 }
