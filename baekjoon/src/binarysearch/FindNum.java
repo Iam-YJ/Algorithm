b4ae15ecbf91772a363802e3de0c443198c955b1
PYJ <yujin353@naver.com>
2023-01-12 12:16:01 +0900

    [20230112] algorithm study - binarySearch 1920

--- baekjoon/src/binarysearch/FindNum.java
+++ baekjoon/src/binarysearch/FindNum.java
@@ -7,13 +7,13 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class FindNum {
+    public static List<Integer> numList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int numCount = Integer.parseInt(br.readLine());
-        List<Integer> numList = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < numCount; i++) {
             numList.add(Integer.parseInt(st.nextToken()));
@@ -24,13 +24,26 @@ public class FindNum {
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < findNumCount; i++) {
             int num = Integer.parseInt(st.nextToken());
-            if (numList.contains(num)) {
-                bw.write(1 + "\n");
-            } else {
-                bw.write(0 + "\n");
-            }
+            bw.write(binarySearch(num) + "\n");
         }
         bw.flush();
+    }
 
+    public static int binarySearch(int num) {
+        int left = 0;
+        int right = numList.size() - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (num == numList.get(mid)) {
+                return 1;
+            } else if (num < numList.get(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return 0;
     }
 }
