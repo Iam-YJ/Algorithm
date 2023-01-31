73c0707372635c68caed17d4a8769c09b205b05a
Iam-YJ <yujin353@naver.com>
2023-01-31 23:38:43 +0900

    [20230131] algorithm study - greedy 1213

---
+++ baekjoon/src/study/week5/greedy/Number1213.java
@@ -0,0 +1,53 @@
+package study.week5.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number1213 {
+    static int[] alpha = new int[26];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            alpha[ch - 65] += 1;
+        }
+
+        int oddCount = 0;
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] % 2 != 0) {
+                oddCount += 1;
+            }
+        }
+
+        if (oddCount > 1) {
+            System.out.println("I'm Sorry Hansoo");
+        } else {
+            Greedy();
+        }
+    }
+
+    public static void Greedy() {
+        String front = "";
+        String mid = "";
+        String end = "";
+
+        for (int i = 0; i < alpha.length; i++) {
+            front += Character.toString((char) (65 + i)).repeat(alpha[i]/2);
+        }
+
+        for (int i = 0; i < alpha.length; i++) {
+            if (alpha[i] % 2 != 0) {
+                mid += ((char) (65 + i));
+            }
+        }
+
+        String reversedFront = new StringBuffer(front).reverse().toString();
+        end = reversedFront;
+
+        System.out.println(front + mid + end);
+    }
+}
